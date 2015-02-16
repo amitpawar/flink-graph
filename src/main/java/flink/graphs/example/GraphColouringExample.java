71cdc6b2310cb11819ca411dae05524a4245f23a
HungUnicorn <unicorn.banachi@gmail.com>
2015-02-16 11:10:29 +0100

    cluster version

--- src/main/java/flink/graphs/example/GraphColouringExample.java
+++ src/main/java/flink/graphs/example/GraphColouringExample.java
@@ -2,28 +2,17 @@ package flink.graphs.example;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-import java.util.HashSet;
 import java.util.regex.Pattern;
 
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.io.RemoteCollectorConsumer;
 import org.apache.flink.api.java.io.RemoteCollectorImpl;
-import org.apache.flink.api.java.io.RemoteCollectorOutputFormat;
 import org.apache.flink.api.java.operators.DataSource;
-import org.apache.flink.api.java.operators.DeltaIteration;
-import org.apache.flink.api.java.operators.IterativeDataSet;
-import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.core.fs.FileSystem.WriteMode;
 import org.apache.flink.types.NullValue;
@@ -35,20 +24,22 @@ import flink.graphs.Vertex;
 import flink.graphs.library.GraphColouring;
 
 public class GraphColouringExample {
-
+	
+	private static String argPathToArc = "";
+	private static String argPathOut = "";
 	private static int maxiteration;
-
+	
 	public static void main(String[] args) throws Exception {
 
-		// ExecutionEnvironment env =
-		// ExecutionEnvironment.getExecutionEnvironment();
-		ExecutionEnvironment env = ExecutionEnvironment
-				.createLocalEnvironment();
+		if (!parseParameters(args)) {
+			return;
+		}
+		final ExecutionEnvironment env = ExecutionEnvironment
+				.getExecutionEnvironment();
 
 		maxiteration = 10;
-
-		// Id cannot be 0
-		DataSource<String> input = env.readTextFile("/Users/dgll/graph6");
+		
+		DataSource<String> input = env.readTextFile(argPathToArc);
 
 		DataSet<Vertex<Long, Tuple4<Integer, Integer, Integer, Integer>>> nodes = input
 				.flatMap(new NodeReader()).distinct();
@@ -59,54 +50,10 @@ public class GraphColouringExample {
 		Graph<Long, Tuple4<Integer, Integer, Integer, Integer>, NullValue> graph = new Graph<Long, Tuple4<Integer, Integer, Integer, Integer>, NullValue>(
 				nodes, edges, env);
 
-		// Graph<Long, Tuple3<Integer, Integer, Integer>, NullValue>
-		// graphInputMapped = graph
-		// .mapVertices(new InitVerticesMapper());
-
-		// graph.getEdges().print();
-		// DeltaIteration<Graph<Long, Tuple3<Integer, Integer, Integer>,
-		// NullValue>, Graph<Long, Tuple3<Integer, Integer, Integer>,
-		// NullValue>>
-		// iteration = graphInputMapped.
-		// verticesWithInitialId.iterateDelta(verticesWithInitialId,
-		// maxIterations, 0);
-
 		int colour = 0;
-
-		// GraphColouring algorithm = new GraphColouring(maxiteration, colour);
-		// Graph<Long, Tuple3<Integer, Integer, Integer>, NullValue> resultGraph
-		// = graph
-		// .run(algorithm);
-
-		// resultGraph.getVertices().print();
-		// env.execute();
-
 		Graph<Long, Tuple4<Integer, Integer, Integer, Integer>, NullValue> graphFiltered = graph;
-		/*
-		 * while (colour < 4) { System.out.println("Colours:" + colour);
-		 * GraphColouring<Long> algorithm = new
-		 * GraphColouring<Long>(maxiteration, colour); DataSet<Tuple2<Long,
-		 * Long>> degrees = graphFiltered.getDegrees(); graphFiltered =
-		 * graphFiltered.joinWithVertices(degrees, new
-		 * ColourIsolatedNodes<Long>(colour));
-		 * System.out.println("ColourIsolatedNodes");
-		 * graphFiltered.getVertices().print();
-		 * env.execute("First build colour " + colour);
-		 * //graphFiltered.mapVertices(new ColourIsolatedNodes<Long>(colour));
-		 * Graph<Long, Tuple4<Integer, Integer, Integer, Integer>, NullValue>
-		 * resultGraph = graphFiltered.run(algorithm);
-		 * System.out.println("ResultGraph"); resultGraph.getVertices().print();
-		 * env.execute("Second build colour " + colour); graphFiltered =
-		 * resultGraph.filterOnVertices(new FilterVertex());
-		 * 
-		 * DataSet<Vertex<Long, Tuple4<Integer, Integer, Integer, Integer>>>
-		 * result = graphFiltered.getVertices();
-		 * System.out.println("FilteredGraph"); result.print(); //graphFiltered
-		 * = resultGraph; // filtered env.execute("Third build colour " +
-		 * colour);
-		 * 
-		 * // check if the filtered result is empty, if so, break colour++; }
-		 */
+
+		// check if the filtered result is empty, if so, break colour++; }
 
 		int verticesRemaining = 0;
 
@@ -115,36 +62,19 @@ public class GraphColouringExample {
 			GraphColouring<Long> algorithm = new GraphColouring<Long>(
 					maxiteration, colour);
 
-			/*
-			 * DataSet<Tuple2<Long, Long>> degrees = graphFiltered.getDegrees();
-			 * graphFiltered = graphFiltered.joinWithVertices(degrees, new
-			 * ColourIsolatedNodes<Long>(colour));
-			 * System.out.println("ColourIsolatedNodes");
-			 * //graphFiltered.getVertices().print();
-			 * graphFiltered.getVertices()
-			 * .writeAsCsv("/Users/dgll/IT4BI/IMPRO3/secondOut/iso"+colour,
-			 * WriteMode.OVERWRITE); env.execute("First build colour " +
-			 * colour);
-			 */
 			// graphFiltered.mapVertices(new ColourIsolatedNodes<Long>(colour));
 			Graph<Long, Tuple4<Integer, Integer, Integer, Integer>, NullValue> resultGraph = graphFiltered
 					.run(algorithm);
 			System.out.println("ResultGraph");
 			// resultGraph.getVertices().print();
-			resultGraph.getVertices().writeAsCsv(
-					"/Users/dgll/IT4BI/IMPRO3/thirdOut/res" + colour,
+			resultGraph.getVertices().writeAsCsv(argPathOut + colour,
 					WriteMode.OVERWRITE);
 			env.execute("Second build colour " + colour);
 
-			graphFiltered = resultGraph.filterOnVertices(new FilterVetex());
+			graphFiltered = resultGraph.filterOnVertices(new FilterVertex());
 			Graph<Long, Tuple4<Integer, Integer, Integer, Integer>, NullValue> colourGraph = resultGraph
 					.filterOnVertices(new FilterNonColourVertex());
 
-			// DataSet<Vertex<Long, Tuple4<Integer, Integer, Integer, Integer>>>
-			// result = graphFiltered.getVertices();
-			System.out.println("FilteredGraph");
-			// result.print();
-			// graphFiltered = resultGraph; // filtered
 
 			final ArrayList<Integer> collection = new ArrayList<Integer>();
 			DataSet<Integer> num = graphFiltered.numberOfEdges();
@@ -157,8 +87,7 @@ public class GraphColouringExample {
 							collection.add(element);
 						}
 					});
-			colourGraph.getVertices().writeAsCsv(
-					"/Users/dgll/IT4BI/IMPRO3/thirdOut/op" + colour,
+			colourGraph.getVertices().writeAsCsv(argPathOut + colour,
 					WriteMode.OVERWRITE);
 
 			env.execute("Third build colour " + colour);
@@ -179,10 +108,9 @@ public class GraphColouringExample {
 		DataSet<Tuple2<Long, Long>> degrees = graphFiltered.getDegrees();
 		graphFiltered = graphFiltered.joinWithVertices(degrees,
 				new ColourIsolatedNodes<Long>(colour));
-		//System.out.println("ColourIsolatedNodes");
+		// System.out.println("ColourIsolatedNodes");
 		// graphFiltered.getVertices().print();
-		graphFiltered.getVertices().writeAsCsv(
-				"/Users/dgll/IT4BI/IMPRO3/thirdOut/iso" + colour,
+		graphFiltered.getVertices().writeAsCsv(argPathOut + colour,
 				WriteMode.OVERWRITE);
 		env.execute("First build colour " + colour);
 
@@ -217,32 +145,26 @@ public class GraphColouringExample {
 
 	}
 
-	
-	
 	@SuppressWarnings("serial")
-	public static final class FilterVertex
-			implements
-			FilterFunction<Vertex<Long, Tuple4<Integer, Integer, Integer, Integer>>> {
+	public static final class FilterVertex implements
+			FilterFunction<Tuple4<Integer, Integer, Integer, Integer>> {
 
 		@Override
-		public boolean filter(
-				Vertex<Long, Tuple4<Integer, Integer, Integer, Integer>> value)
+		public boolean filter(Tuple4<Integer, Integer, Integer, Integer> value)
 				throws Exception {
 			// TODO Auto-generated method stub
-			return value.f1.f0 == -1;
+			return value.f0 == -1;
 		}
 	}
 
 	@SuppressWarnings("serial")
-	public static final class FilterNonColourVertex
-			implements
-			FilterFunction<Vertex<Long, Tuple4<Integer, Integer, Integer, Integer>>> {
+	public static final class FilterNonColourVertex implements
+			FilterFunction<Tuple4<Integer, Integer, Integer, Integer>> {
 
 		@Override
-		public boolean filter(
-				Vertex<Long, Tuple4<Integer, Integer, Integer, Integer>> value)
+		public boolean filter(Tuple4<Integer, Integer, Integer, Integer> value)
 				throws Exception {
-			return value.f1.f0 != -1;
+			return value.f0 != -1;
 		}
 	}
 
@@ -268,20 +190,6 @@ public class GraphColouringExample {
 		}
 	}
 
-	// public static final class InitVerticesMapper<K extends Comparable<K> &
-	// Serializable>
-	// implements
-	// MapFunction<Vertex<K, Double>, Vertex<Long, Tuple3<Integer, Integer,
-	// Integer>>> {
-	//
-	// public Vertex<Long, Tuple3<Integer, Integer, Integer>> map(Vertex<K,
-	// Double> value) {
-	//
-	//
-	// return new Tuple3<Integer, Integer, Integer>(-1, -1, -1);
-	// }
-	// }
-	//
 	@SuppressWarnings("serial")
 	public static class NodeReader
 			implements
@@ -295,8 +203,7 @@ public class GraphColouringExample {
 				throws Exception {
 			if (!s.startsWith("%")) {
 				String[] tokens = SEPARATOR.split(s);
-
-				long ctr = 0;
+				
 				long source = Long.parseLong(tokens[0]);
 				long target = Long.parseLong(tokens[1]);
 
@@ -312,6 +219,21 @@ public class GraphColouringExample {
 										-1, -1, -1, -1)));
 			}
 		}
+	}
+
+	public static boolean parseParameters(String[] args) {
+
+		if (args.length < 3 || args.length > 3) {
+			System.err
+					.println("Usage: [path to arc file] [output path] [maxIterations]");
+			return false;
+		}
+
+		argPathToArc = args[0];		
+		argPathOut = args[1];
+		maxiteration = Integer.parseInt(args[2]);
+
+		return true;
 	}
 
 }
