49ae5477af6e65f367c72c6a8cdfc3a08731985f
amitpawar <sunny538889@hotmail.com>
2015-02-20 19:39:04 +0100

    Test logic change

--- src/test/java/flink/graphs/TestGraphColouring.java
+++ src/test/java/flink/graphs/TestGraphColouring.java
@@ -4,17 +4,11 @@ package flink.graphs;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.regex.Pattern;
-
 import org.junit.*;
-
 import flink.graphs.example.GCExample;
 
 
@@ -22,7 +16,7 @@ public class TestGraphColouring {
 
 	private Map<Integer, Integer> colorMap;
 	
-	private String inputFilePath = "/home/amit/impro/colortest3";
+	private String inputFilePath = "src/main/java/test/colortest4";
 	private String outputFolderPath = "/home/amit/impro/testColor3OP";
 	private String maxIterations = "10"; 
 	
@@ -36,6 +30,7 @@ public class TestGraphColouring {
 		
 		colorMap = new HashMap();
 		String line;
+		Pattern vertexGetter = Pattern.compile(",");
 		int i = 0;
         for (File fileEntry : outputDirectory.listFiles()) {
         	if(fileEntry.isDirectory()){
@@ -46,7 +41,8 @@ public class TestGraphColouring {
         			{
         				BufferedReader br = new BufferedReader(new FileReader(insideFile));
 			            while ((line = br.readLine()) != null) {
-			               int vertexId = Integer.parseInt(line.substring(0, 1));	
+			               String[] breakLine = vertexGetter.split(line);
+			               int vertexId = Integer.parseInt(breakLine[0]);	
 			               colorMap.put(vertexId, i);
 			            }
 			            br.close();
@@ -68,6 +64,8 @@ public class TestGraphColouring {
 			int target = Integer.parseInt(tokens[1]);
 			
 			junit.framework.TestCase.assertFalse(colorMap.get(source) == colorMap.get(target));
+			if(colorMap.get(source) == colorMap.get(target))
+				System.out.println(source + " "+target);
         }
         
 	}
